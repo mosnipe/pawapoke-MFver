be39b94bf9c871fee3bb2fb0b0875657ddbb043d
mosnipe_win <sniya.pagone824055@outlook.jp>
2024-11-07 22:17:28 +0900

    <span class='blink'>▼</span>削除

--- dialogs.js
+++ dialogs.js
@@ -1,45 +1,45 @@
 // dialogs.js
 export const dialogs = {
     nishikawa: [
-        { text: "西川さんです➡ <span class='blink'>▼</span>", speaker: "right", imagePath: "./img/nishikawasan.gif" },
-        { text: "西川さんに挨拶しなきゃ <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/main.gif" },
-        { text: "西川さん。今まで本当にお世話になりました！ <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/handsup.gif" },
-        { text: "お疲れ！次の会社でも頑張るダニよ <span class='blink'>▼</span>", speaker: "right", imagePath: "./img/nishikawasan.gif" },
-        { text: "次のステップに進む勇気をもらった気がする。 <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/smile.gif" }
+        { text: "西川さんです➡", speaker: "right", imagePath: "./img/nishikawasan.gif" },
+        { text: "西川さんに挨拶しなきゃ", speaker: "left", imagePath: "./img/main.gif" },
+        { text: "西川さん。今まで本当にお世話になりました！", speaker: "left", imagePath: "./img/handsup.gif" },
+        { text: "お疲れ！次の会社でも頑張るダニよ", speaker: "right", imagePath: "./img/nishikawasan.gif" },
+        { text: "次のステップに進む勇気をもらった気がする。", speaker: "left", imagePath: "./img/smile.gif" }
     ],
     murata: [
-        { text: "お疲れ！ <span class='blink'>▼</span>", speaker: "right", imagePath: "./img/muratasan.gif" },
-        { text: "村田さん！今までありがとうございました！ <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/handsup.gif" },
-        { text: "よっしー、東京行ったらホテルマンやり！ <span class='blink'>▼</span>", speaker: "right", imagePath: "./img/muratasan.gif" },
-        { text: "村田さんの言葉に元気をもらった。 <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/wow.gif" }
+        { text: "お疲れ！", speaker: "right", imagePath: "./img/muratasan.gif" },
+        { text: "村田さん！今までありがとうございました！", speaker: "left", imagePath: "./img/handsup.gif" },
+        { text: "よっしー、東京行ったらホテルマンやり！", speaker: "right", imagePath: "./img/muratasan.gif" },
+        { text: "村田さんの言葉に元気をもらった。", speaker: "left", imagePath: "./img/wow.gif" }
     ],
     nishimura: [
-        { text: "西村さんです→ <span class='blink'>▼</span>", speaker: "right", imagePath: "./img/nishimurasan.gif" },
-        { text: "西村さんに挨拶しなきゃ <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/main.gif" },
-        { text: "西村さん。今までありがとうございました。 <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/handsup.gif" },
-        { text: "都産技研の際は本当に支えていただいて助かりました <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/handsup.gif" },
-        { text: "お疲れさま。次の会社でも頑張ってね <span class='blink'>▼</span>", speaker: "right", imagePath: "./img/nishimurasan.gif" },
-        { text: "西村さんの笑顔が心に残った。 <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/smile.gif" }
+        { text: "西村さんです→", speaker: "right", imagePath: "./img/nishimurasan.gif" },
+        { text: "西村さんに挨拶しなきゃ", speaker: "left", imagePath: "./img/main.gif" },
+        { text: "西村さん。今までありがとうございました。", speaker: "left", imagePath: "./img/handsup.gif" },
+        { text: "都産技研の際は本当に支えていただいて助かりました", speaker: "left", imagePath: "./img/handsup.gif" },
+        { text: "お疲れさま。次の会社でも頑張ってね", speaker: "right", imagePath: "./img/nishimurasan.gif" },
+        { text: "西村さんの笑顔が心に残った。", speaker: "left", imagePath: "./img/smile.gif" }
     ],
     makino: [
-        { text: "牧野さんです→ <span class='blink'>▼</span>", speaker: "right", imagePath: "./img/makinosan.png" },
-        { text: "牧野さんに挨拶しなきゃ <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/main.gif" },
-        { text: "牧野さん、今までありがとうございました。牧野さんなしに僕のMF生活は語れません <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/cry.gif" },
-        { text: "おつ！東京にのまれるなよ。まめたのためにもちゃんと実家に帰るんやで <span class='blink'>▼</span>", speaker: "right", imagePath: "./img/makinosan.png" },
-        { text: "はい！ <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/confused.gif" }
+        { text: "牧野さんです→", speaker: "right", imagePath: "./img/makinosan.png" },
+        { text: "牧野さんに挨拶しなきゃ", speaker: "left", imagePath: "./img/main.gif" },
+        { text: "牧野さん、今までありがとうございました。牧野さんなしに僕のMF生活は語れません", speaker: "left", imagePath: "./img/cry.gif" },
+        { text: "おつ！東京にのまれるなよ。まめたのためにもちゃんと実家に帰るんやで", speaker: "right", imagePath: "./img/makinosan.png" },
+        { text: "はい！", speaker: "left", imagePath: "./img/confused.gif" }
     ],
     uda: [
-        { text: "宇田さん？です→ <span class='blink'>▼</span>", speaker: "right", imagePath: "./img/udasan.gif" },
-        { text: "宇田さんに挨拶しなきゃ <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/main.gif" },
-        { text: "宇田さん、今までありがとうございました。関西MSの件では凄くお世話になりました。 <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/handsup.gif" },
-        { text: "サマデース。吉岡君は体力だけはあるから、その調子で頑張りや <span class='blink'>▼</span>", speaker: "right", imagePath: "./img/udasan.gif" },
-        { text: "宇田さんの教えを胸に次へ進もうと思った。 <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/determined.gif" }
+        { text: "宇田さん？です→", speaker: "right", imagePath: "./img/udasan.gif" },
+        { text: "宇田さんに挨拶しなきゃ", speaker: "left", imagePath: "./img/main.gif" },
+        { text: "宇田さん、今までありがとうございました。関西MSの件では凄くお世話になりました。", speaker: "left", imagePath: "./img/handsup.gif" },
+        { text: "サマデース。吉岡君は体力だけはあるから、その調子で頑張りや", speaker: "right", imagePath: "./img/udasan.gif" },
+        { text: "宇田さんの教えを胸に次へ進もうと思った。", speaker: "left", imagePath: "./img/determined.gif" }
     ],
     kuwama: [
-        { text: "桑名さんです→ <span class='blink'>▼</span>", speaker: "right", imagePath: "./img/kuwana01.gif" },
-        { text: "桑名さんに挨拶しなきゃ <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/main.gif" },
-        { text: "みゆせるさん、今までお世話になりました！みゆせるさんは僕の憧れの先輩です <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/handsup.gif" },
-        { text: "おっつー。まぁまた東京でたいがと3人でのも！ <span class='blink'>▼</span>", speaker: "right", imagePath: "./img/kuwana02.gif" },
-        { text: "東京の酒が上手い店を探さなきゃ <span class='blink'>▼</span>", speaker: "left", imagePath: "./img/smile.gif" }
+        { text: "桑名さんです→", speaker: "right", imagePath: "./img/kuwana01.gif" },
+        { text: "桑名さんに挨拶しなきゃ", speaker: "left", imagePath: "./img/main.gif" },
+        { text: "みゆせるさん、今までお世話になりました！みゆせるさんは僕の憧れの先輩です", speaker: "left", imagePath: "./img/handsup.gif" },
+        { text: "おっつー。まぁまた東京でたいがと3人でのも！", speaker: "right", imagePath: "./img/kuwana02.gif" },
+        { text: "東京の酒が上手い店を探さなきゃ", speaker: "left", imagePath: "./img/smile.gif" }
     ]
 };
