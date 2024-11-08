790196d94583bbc2536fcea9fecdbde324d448c4
mosnipe_win <sniya.pagone824055@outlook.jp>
2024-11-08 11:18:27 +0900

    城阪さん・野口さん追加

--- js/dialogs.js
+++ js/dialogs.js
@@ -3,51 +3,56 @@ export const dialogs = {
     nishikawa: [
         { text: "西川さんです➡", speaker: "right", imagePath: "./img/nishikawasan.gif" },
         { text: "西川さんに挨拶しなきゃ", speaker: "left", imagePath: "./img/main.gif" },
-        { text: "西川さん。今まで本当にお世話になりました！", speaker: "left", imagePath: "./img/handsup.gif" },
+        { text: "西川さん。今まで本当にお世話になりました！配属されてから今日の今日までお世話になりっぱなしでした…この間少しでも西川さんのお役に立てていたら何よりです。次の会社でも学んだことを精一杯発揮しようと思います！", speaker: "left", imagePath: "./img/handsup.gif" },
         { text: "お疲れ！次の会社でも頑張るダニよ", speaker: "right", imagePath: "./img/nishikawasan.gif" },
         { text: "次のステップに進む勇気をもらった気がする。", speaker: "left", imagePath: "./img/smile.gif" }
     ],
     murata: [
         { text: "村田さん、お疲れ様でした！", speaker: "right", imagePath: "./img/muratasan.gif" },
-        { text: "村田さん！今までありがとうございました！", speaker: "left", imagePath: "./img/handsup.gif" },
+        { text: "村田さん！今までありがとうございました！中プラの案件では何回も一緒に名古屋やいろんな場所に行ってサポートしていただいたり、時には僕の事を思って叱ってくださって本当に感謝しています！", speaker: "left", imagePath: "./img/handsup.gif" },
         { text: "よっしー、東京行ったらホテルマンやり！", speaker: "right", imagePath: "./img/muratasan.gif" },
         { text: "村田さんの言葉に元気をもらった。", speaker: "left", imagePath: "./img/wow.gif" }
     ],
     nishimura: [
         { text: "西村さんに挨拶しなきゃ", speaker: "right", imagePath: "./img/nishimurasan.gif" },
-        { text: "西村さん、今までありがとうございました。", speaker: "left", imagePath: "./img/main.gif" },
-        { text: "都産技研の際は本当に助けられました", speaker: "left", imagePath: "./img/handsup.gif" },
+        { text: "西村さん、今までありがとうございました。いつも西村さんのやり切る姿勢やクオリティにこだわるところを見ていて、僕には全然ない部分だったので尊敬していました。", speaker: "left", imagePath: "./img/main.gif" },
+        { text: "また、都産技研の際は本当に助けられました。路頭に迷って精神的につらいとき、いつも心配してくださって助けられていました。", speaker: "left", imagePath: "./img/handsup.gif" },
         { text: "お疲れさま。次の会社でも頑張ってね", speaker: "right", imagePath: "./img/nishimurasan.gif" },
         { text: "西村さんの笑顔が心に残った。", speaker: "left", imagePath: "./img/smile.gif" }
     ],
     makino: [
-        { text: "牧野さん、感謝を伝えなきゃ", speaker: "right", imagePath: "./img/makinosan.png" },
-        { text: "牧野さん、今までありがとうございました。", speaker: "left", imagePath: "./img/cry.gif" },
-        { text: "おつ！東京にのまれるなよ。", speaker: "right", imagePath: "./img/makinosan.png" },
+        { text: "牧野さん→", speaker: "right", imagePath: "./img/makinosan.gif" },
+        { text: "牧野さん、今までありがとうございました。ほとんどの案件で僕は牧野さんに助けていただいて牧野さんがいなかったら今どうなっていたのかというくらい助けられてきました。", speaker: "left", imagePath: "./img/cry.gif" },
+        { text: "牧野さんと一緒にやった道場で学んだこと、地頭力や考える力を今後の人生でも活かしていきます！", speaker: "left", imagePath: "./img/determined.gif" },
+        { text: "おつ！東京にのまれるなよ。まめたのためにもちゃんと実家にも帰るんやで", speaker: "right", imagePath: "./img/makinosan.gif" },
         { text: "はい！しっかりやります", speaker: "left", imagePath: "./img/confused.gif" }
     ],
     uda: [
         { text: "宇田さんに挨拶しなきゃ", speaker: "right", imagePath: "./img/udasan.gif" },
-        { text: "宇田さん、今までありがとうございました。", speaker: "left", imagePath: "./img/main.gif" },
-        { text: "吉岡君は体力だけはあるな！", speaker: "right", imagePath: "./img/udasan.gif" },
-        { text: "その通りです。頑張ります。", speaker: "left", imagePath: "./img/determined.gif" }
+        { text: "宇田さん、今までありがとうございました。宇田さんには仕事でのコツや姿勢について色々と教えていただきました。", speaker: "left", imagePath: "./img/main.gif" },
+        { text: "実は罫線の色をちょっと薄い黒にするとか、paddingを偶数にするとかそういったことも教えてもらっているのを今でも実践しています。", speaker: "left", imagePath: "./img/main.gif" },
+        { text: "サマデース。吉岡君は体力あるからその調子で頑張りや", speaker: "right", imagePath: "./img/udasan.gif" },
+        { text: "はい！頑張ります！！", speaker: "left", imagePath: "./img/determined.gif" }
     ],
     kuwama: [
-        { text: "桑名さん、感謝を込めて…", speaker: "right", imagePath: "./img/kuwana01.gif" },
-        { text: "桑名さん、今までありがとうございました！", speaker: "left", imagePath: "./img/handsup.gif" },
-        { text: "まぁまた東京で飲もうよ！", speaker: "right", imagePath: "./img/kuwana02.gif" },
+        { text: "桑名さんです→", speaker: "right", imagePath: "./img/kuwana01.gif" },
+        { text: "みゆせるさん、今までありがとうございました！みゆせるさんは入社してからずっと尊敬している先輩で、一緒のチームに来てくれた時めちゃくちゃ嬉しかったです！", speaker: "left", imagePath: "./img/handsup.gif" },
+        { text: "チームが一緒になってからは、僕の仕事をめっちゃ手伝ってくださったり、しょーもない話を笑ってくれたりして、めちゃくちゃ助かっていました", speaker: "left", imagePath: "./img/cry.gif" },
+        { text: "おつー！また帰ってきたときは飲もう！", speaker: "right", imagePath: "./img/kuwana02.gif" },
         { text: "ぜひお願いします！", speaker: "left", imagePath: "./img/smile.gif" }
     ],
     yamada: [
-        { text: "山田さんに挨拶しなきゃ", speaker: "right", imagePath: "./img/yamadasan.gif" },
-        { text: "山田さん、今まで本当にお世話になりました。", speaker: "left", imagePath: "./img/main.gif" },
-        { text: "こちらこそお疲れ様、次もがんばれ！", speaker: "right", imagePath: "./img/yamadasan.gif" },
+        { text: "城阪さんです→", speaker: "right", imagePath: "./img/shirosaka.gif" },
+        { text: "城阪さん、今までありがとうございました。城阪さんには入社して以降、凄く気にかけてくださってて、出社された時はよくお話を聞いてもらいました。", speaker: "left", imagePath: "./img/main.gif" },
+        { text: "案件のこととか色々アドバイスもいただいたり、プログラミングのカリキュラムまで組んでいただいたり、助けていただいたりして本当に感謝しています。", speaker: "left", imagePath: "./img/cry.gif" },
+        { text: "お疲れ様です。吉岡さんのご活躍を期待してますよ", speaker: "right", imagePath: "./img/shirosaka.gif" },
         { text: "はい、精一杯頑張ります！", speaker: "left", imagePath: "./img/determined.gif" }
     ],
     suzuki: [
-        { text: "鈴木さんにも挨拶しとこう", speaker: "right", imagePath: "./img/suzukisan.gif" },
-        { text: "鈴木さん、ありがとうございました！", speaker: "left", imagePath: "./img/main.gif" },
-        { text: "よっしー、これからも期待してるぞ！", speaker: "right", imagePath: "./img/suzukisan.gif" },
-        { text: "ありがとうございます！精進します。", speaker: "left", imagePath: "./img/confused.gif" }
+        { text: "野口さん→", speaker: "right", imagePath: "./img/noguchisan.gif" },
+        { text: "野口さん、今までありがとうございました！　僕は野口さんの姿を見てこんなエンジニアになりたいと思ってMFに入社しました！", speaker: "left", imagePath: "./img/main.gif" },
+        { text: "形は違えど少しでも野口さんみたいな人になれたらと思ってここまでやってきました！お互い次のステージでも頑張りましょう！", speaker: "left", imagePath: "./img/handsup.gif" },
+        { text: "お疲れ様！ 22卒の中で最初の卒業者やね。次の環境でも頑張れ！", speaker: "right", imagePath: "./img/noguchisan.gif" },
+        { text: "ありがとうございます！精進します！", speaker: "left", imagePath: "./img/determined.gif" }
     ]
 };
